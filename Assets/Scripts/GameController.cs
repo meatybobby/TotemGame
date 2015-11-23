68ed9888c055ade8e23d22aa8867494f8a95a7c2
DouglasWu <dgrey1116@gmail.com>
2015-11-23 17:33:15 +0800

    有2X2怪物prefab

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -26,11 +26,11 @@ public class GameController : MonoBehaviour {
 			if(enemyCount>=enemyNum) yield break;
 			IntVector2 newPos = new IntVector2(4,4);
 			Vector3 enemyPosition = Map.GetRealPosition(newPos);
-			enemyPosition.x += Map.unitCell/2;
-			enemyPosition.y += Map.unitCell/2;
+			//enemyPosition.x += Map.unitCell/2;
+			//enemyPosition.y += Map.unitCell/2;
 			Quaternion enemyRotation = Quaternion.Euler(0f,0f,-90.0f);
-			Instantiate(monster1, enemyPosition , enemyRotation);
-			Enemy enemy = monster1.GetComponent<Enemy>();
+			Instantiate(monster2, enemyPosition , enemyRotation);
+			Enemy enemy = monster2.GetComponent<Enemy>();
 			enemy.pos = newPos;
 			enemy.Rotate(enemy.dir);
 			enemyCount++;
